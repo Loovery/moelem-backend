dd25f1d38c82ebceea4acc52aa8afc201b0ea059
Grigorii Korepanov <loovery@Grigoriis-MacBook-Air.local>
2019-10-14 17:50:24 +0500

    fix close event again

--- src/apps/events/services/closeEvent.js
+++ src/apps/events/services/closeEvent.js
@@ -1,8 +1,8 @@
-import { User } from 'src/db/models';
+import { Event } from 'src/db/models';
 import to from 'await-to-js';
 
 export default async (id) => {
-  const [error, data] = await to(User.updateOne({ id }, { $set: { isClosed: true } }));
+  const [error, data] = await to(Event.updateOne({ id }, { $set: { isClosed: true } }));
 
   if (error) throw new Error(error);
   return data;
